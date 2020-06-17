b850c86b2f194e1b23dc21095d94c00610a8fbe1
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-17 09:51:33 +0200

    comienzo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Bienvenidos a este curso</h2>
+        </div>
+        <h3>Vamos a aprender React</h3>
+        <p>Hola mundo, estoy usando React</p>
+        <strong>Esto es un strong</strong>
+      </div>
+    );
+  }
 }
 
 export default App;
