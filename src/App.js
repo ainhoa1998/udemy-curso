d3ecd8f546f22d174692ada1eaf417901a7466e1
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-18 08:30:04 +0200

    cursos hasta 33

--- src/App.js
+++ src/App.js
@@ -1,18 +1,113 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ConditionalSection from "./sections/conditional";
+import cars from "./data/cars.json";
+
+/*function Hello(props) {
+  return <h2>{props.title}</h2>;
+} */
+
+//const Hello = (props) => <h2>{props.title}</h2>;
+
+class Hello extends Component {
+  render() {
+    return <h2>{this.props.title}</h2>;
+  }
+}
+
+class Text extends Component {
+  render() {
+    const { arrayOfNumbers, objectWithInfo, multiply, title } = this.props;
+
+    const mapped = arrayOfNumbers.map(multiply);
+
+    return (
+      <div>
+        {title}
+        <p>{mapped.join(", ")}</p>
+        <p>{objectWithInfo.key}</p>
+      </div>
+    );
+  }
+}
+
+class Contador extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: this.props.contadorInicial,
+    };
+    setInterval(() => {
+      this.setState({
+        contador: this.state.contador + 1,
+      });
+    }, 1000);
+  }
+  render() {
+    return <ContadorNumero numero={this.state.contador} />;
+  }
+}
+
+Contador.defaultProps = { contadorInicial: 5 };
+
+class ContadorNumero extends Component {
+  render() {
+    return <span>{this.props.numero}</span>;
+  }
+}
+
+class CarItem extends Component {
+  render() {
+    return cars.map((car) => {
+      return (
+        <li key={car.id}>
+          <p>
+            <strong>Nombre:</strong> {car.name}
+          </p>
+          <p>
+            <strong>Marca:</strong> {car.company}
+          </p>
+        </li>
+      );
+    });
+  }
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mouseX: 0,
+      mouseY: 0,
+    };
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+  handleClick(e) {
+    alert("Hi, here!");
+  }
+
+  handleMouseMove(e) {
+    const { clienteX, clienteY } = e;
+    this.setState({
+      mouseX: clienteX,
+      mouseY: clienteY,
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Bienvenidos a este curso</h2>
+        <h4>Eventos</h4>
+        <button onClick={this.handleClick}>Hi there!</button>
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{ border: "1px solid #000", marginTop: 10, padding: 10 }}
+        >
+          <p>
+            {this.state.mouseX},{this.state.mouseY}
+          </p>
         </div>
-        <h3>Vamos a aprender React</h3>
-        <p>Hola mundo, estoy usando React</p>
-        <strong>Esto es un strong</strong>
       </div>
     );
   }
---
+++ src/sections/conditional.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class LoginButton extends Component {
+  render() {
+    return <button>Iniciar sesión</button>;
+  }
+}
+
+class LogoutButton extends Component {
+  render() {
+    return (
+      <div>
+        <p>Bienvenido usuario!</p>
+        <button>Cerrar sesión</button>
+      </div>
+    );
+  }
+}
+
+export default class ConditionalSection extends Component {
+  constructor() {
+    super();
+    this.state = { isUserLog: false };
+  }
+  render() {
+    return (
+      <div>
+        <h4>Conditional Rendering</h4>
+        {this.state.isUserLog ? <LogoutButton /> : <LogoutButton />}
+      </div>
+    );
+  }
+}
